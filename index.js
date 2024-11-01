6395cdd0c0dd229e6cf44c7569b94fad86773ed6
Daniel Aldo <aldo@aptika.co.id>
2024-11-01 15:09:43 +0700

    adjust method for check-digit to get

--- index.js
+++ index.js
@@ -42,42 +42,39 @@ app.get('/pokemon/:name', (req, res) => {
   });
 })
 
-app.post('/check-digit', (req, res) => {
+app.get('/check-digit', (req, res) => {
   try {
-    if(!req.body) throw new Error('body empty')
-    if(!req.body.pan) throw new Error('pan empty')
-    const pan = req.body.pan
+    if (!req.query.pan) throw new Error('PAN query parameter is missing');
+    const pan = req.query.pan;
     
-    function checkDigit (number) {
-      var stack = 0;
+    function checkDigit(number) {
+      let stack = 0;
       number = ("" + number).split('').reverse();
-      var tempn;
-      var tempsum = 0;
-      for (var [key, value] of Object.entries(number)) {
-          if (key % 2 == 0) {
-              tempn = ("" + (parseInt(value) * 2)).split('');
-              tempsum = 0;
-              for (tjum of tempn) tempsum += parseInt(tjum);
-              value = tempsum;
-          }
+      let tempn, tempsum = 0;
+      for (const [key, value] of Object.entries(number)) {
+        if (key % 2 == 0) {
+          tempn = ("" + (parseInt(value) * 2)).split('');
+          tempsum = tempn.reduce((sum, tjum) => sum + parseInt(tjum), 0);
+          stack += tempsum;
+        } else {
           stack += parseInt(value);
+        }
       }
-    
-      var temp = stack;
+      
+      const temp = stack;
       stack %= 10;
-    
-      if (stack != 0) stack -= 10;
-    
-      if ((temp + Math.abs(stack)) % 10 != 0) return false;
-      else return Math.abs(stack);
+      
+      if (stack !== 0) stack -= 10;
+      
+      return (temp + Math.abs(stack)) % 10 === 0 ? Math.abs(stack) : false;
     }
 
-    const result = checkDigit(pan)
-    res.status(200).json(result)
+    const result = checkDigit(pan);
+    res.status(200).json({ result });
   } catch (error) {
-    res.send(error)
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
