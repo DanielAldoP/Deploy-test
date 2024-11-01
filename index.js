fda5415a33ceaf7fec27f623eaa289b290e2dbf2
Daniel Aldo <aldo@aptika.co.id>
2024-11-01 15:02:21 +0700

    add check-digit

--- index.js
+++ index.js
@@ -42,6 +42,43 @@ app.get('/pokemon/:name', (req, res) => {
   });
 })
 
+app.post('/check-digit', (req, res) => {
+  try {
+    if(!req.body) throw new Error('body empty')
+    if(!req.body.pan) throw new Error('pan empty')
+    const pan = req.body.pan
+    
+    function checkDigit (number) {
+      var stack = 0;
+      number = ("" + number).split('').reverse();
+      var tempn;
+      var tempsum = 0;
+      for (var [key, value] of Object.entries(number)) {
+          if (key % 2 == 0) {
+              tempn = ("" + (parseInt(value) * 2)).split('');
+              tempsum = 0;
+              for (tjum of tempn) tempsum += parseInt(tjum);
+              value = tempsum;
+          }
+          stack += parseInt(value);
+      }
+    
+      var temp = stack;
+      stack %= 10;
+    
+      if (stack != 0) stack -= 10;
+    
+      if ((temp + Math.abs(stack)) % 10 != 0) return false;
+      else return Math.abs(stack);
+    }
+
+    const result = checkDigit(pan)
+    res.status(200).json(result)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
